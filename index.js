00046fd6698ddde21f21a08e2dfdb66f4f0799cc
J.S <pachipico1@gmail.com>
2021-04-15 14:37:33 +0900

    complete login post route, fix previous problems

--- index.js
+++ index.js
@@ -27,39 +27,36 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/login", (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: "no email found.",
-      });
-    } else if (user) {
-      user.comparePassword(req.body.password, (err, isMatch) => {
-        if (!isMatch) {
-          return res.json({ loginSuccess: false, message: "wrong password" });
-        } else if (isMatch) {
-          user.generateToken((err, user) => {
-            if (err) return res.status(400).send(err);
-
-            // save token cookie or local storage
-            res
-              .cookie("x_auth", user.token)
-              .status(200)
-              .json({ loginSuccess: true, userId: user._id });
-          });
-        }
-      });
-    }
-  });
-});
-
 app.post("/register", (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //find requested email in our database
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({ success: false, message: "no requested email found" });
+    }
+    //if requested email exist, check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "wrongpassword" });
+      //if password right, create token
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 
